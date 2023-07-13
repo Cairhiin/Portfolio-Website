b2e01e9a2b6a594022eb63932ec033ad9a17b352
Frank van de Voorde <daemonaz@kymp.net>
2023-07-13 18:20:01 +0300

    Add some initial styling and markup

---
+++ src/App.tsx
@@ -0,0 +1,84 @@
+function App() {
+  return (
+    <>
+      <header>
+        <nav className="nav">
+          <h1>
+            <span>{"<h1>"}</span>Frank van de Voorde<span>{"</h1>"}</span>
+          </h1>
+          <ul>
+            <li>About</li>
+            <li>Skills</li>
+            <li>Projects</li>
+            <li>Contact</li>
+          </ul>
+        </nav>
+        <section id="about" className="hero">
+          <div>I am Frank van de Voorde, a Web Developer.</div>
+          <div>
+            <p>
+              A self taught web developer and programmer with a bachelor in
+              Business Administration, exploring both frontend and full-stack
+              development.
+            </p>
+
+            <p>
+              Coding is my passion and I aim to learn new things on a daily
+              basis.
+            </p>
+            <div>
+              <p>
+                Years of experience designing, developing and updating a
+                Wordpress site for a local newspaper.
+              </p>
+
+              <p>
+                Check out my projects, skills, GitHub, GitLab, or drop me a
+                line. I am available for freelance opportunities.
+              </p>
+            </div>
+          </div>
+        </section>
+      </header>
+      <main>
+        <section id="skills">
+          Aspernatur ad maxime placeat porro ipsam beatae. Provident
+          voluptatibus veniam ad, dignissimos alias fuga ducimus tenetur illum
+          hic sequi doloremque sint eius esse reiciendis ullam? Amet accusamus
+          illo sequi totam molestiae debitis facere aliquid excepturi odit
+          dicta! Voluptatem voluptates quos, non voluptatibus tenetur animi unde
+          vero maiores nam consectetur numquam dolorem, repellat, debitis culpa
+          optio. Alias dignissimos quaerat architecto aliquam amet deserunt
+          suscipit. Voluptatem dicta voluptatum quas ea at voluptate,
+          voluptatibus provident vero ipsa saepe! Quas beatae placeat nobis ab
+          iste similique inventore impedit dicta.
+        </section>
+        <section id="projects">
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nesciunt,
+          similique deserunt! Delectus facere architecto deserunt in distinctio
+          illo quam, libero sit repudiandae voluptatem repellendus enim ratione
+          nostrum eos sapiente voluptate. Neque dolor quis ipsa error sit ullam
+          assumenda architecto necessitatibus, vitae consectetur magni
+          perferendis sunt temporibus, at ratione nihil autem. Ipsum facere
+          eveniet dicta repellat incidunt et quas. Nulla, fugiat. Exercitationem
+          provident nisi hic quae?
+        </section>
+        <section id="contact">
+          Aspernatur ad maxime placeat porro ipsam beatae. Provident
+          voluptatibus veniam ad, dignissimos alias fuga ducimus tenetur illum
+          hic sequi doloremque sint eius esse reiciendis ullam? Amet accusamus
+          illo sequi totam molestiae debitis facere aliquid excepturi odit
+          dicta! Voluptatem voluptates quos, non voluptatibus tenetur animi unde
+          vero maiores nam consectetur numquam dolorem, repellat, debitis culpa
+          optio. Alias dignissimos quaerat architecto aliquam amet deserunt
+          suscipit. Voluptatem dicta voluptatum quas ea at voluptate,
+          voluptatibus provident vero ipsa saepe! Quas beatae placeat nobis ab
+          iste similique inventore impedit dicta.
+        </section>
+      </main>
+      <footer></footer>
+    </>
+  );
+}
+
+export default App;
