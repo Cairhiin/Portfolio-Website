a4d5828c1a72dd2d034093e7b510f1ff2c78898b
Frank van de Voorde <daemonaz@kymp.net>
2023-08-04 14:17:54 +0300

    Finalize contact form component

--- src/components/ContactForm/index.tsx
+++ src/components/ContactForm/index.tsx
@@ -1,17 +1,21 @@
 import { useForm, SubmitHandler } from "react-hook-form";
 import { yupResolver } from "@hookform/resolvers/yup";
 import * as yup from "yup";
-// import emailjs from "@emailjs/browser";
-// import apiData from "./contact-api-data";
+import emailjs from "@emailjs/browser";
+import apiData from "./contact-api-data";
 import "./index.css";
+import { useState } from "react";
 
 const schema = yup
   .object({
-    name: yup.string().min(3, "must be at least 3 characters long").required(),
+    name: yup
+      .string()
+      .min(3, "name must be at least 3 characters long")
+      .required(),
     email: yup.string().email().required(),
     message: yup
       .string()
-      .min(10, "must be at least 10 characters long")
+      .min(10, "message must be at least 10 characters long")
       .required(),
   })
   .required();
@@ -23,43 +27,63 @@ const ContactForm: () => JSX.Element = () => {
     message: string;
   }
 
+  const [isSubmitted, setIsSubmitted] = useState<boolean>(false);
+
   const {
     register,
+    reset,
     handleSubmit,
     formState: { errors },
   } = useForm<FormValues>({
     resolver: yupResolver(schema),
   });
-  const onSubmit: SubmitHandler<FormValues> = (data) => console.log(data);
 
-  /* const sendEmail = (e: FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
+  const onSubmit: SubmitHandler<FormValues> = ({ name, email, message }) => {
+    const templateParams = {
+      name: name,
+      email: email,
+      message: message,
+    };
 
-    if (form.current == null) return;
     emailjs
-      .sendForm(apiData.service, apiData.template, form.current, apiData.key)
+      .send(apiData.service, apiData.template, templateParams, apiData.key)
       .then(
         (result) => {
-          setFormInfo({ ...formInfo, error: false });
+          console.log("Success!", result);
+          setIsSubmitted((prevState) => !prevState);
+          reset();
+          setTimeout(() => {
+            setIsSubmitted((prevState) => !prevState);
+          }, 5000);
         },
         (error) => {
-          setFormInfo({ ...formInfo, error: true });
+          console.error("Err!", error);
+          setIsSubmitted(false);
         }
-      )
-      .finally(setTimer);
-  }; */
+      );
+  };
 
   return (
     <div id="contact-form">
       <form onSubmit={handleSubmit(onSubmit)}>
+        <label htmlFor="name">Name</label>
         <input {...register("name")} className="form-control" />
-        <p>{errors.name?.message}</p>
+        {!!errors.name?.message && <p>{errors.name?.message}</p>}
+        <label htmlFor="name">Email</label>
         <input {...register("email")} className="form-control" />
-        <p>{errors.email?.message}</p>
+        {!!errors.email?.message && <p>{errors.email?.message}</p>}
+        <label htmlFor="name">Message</label>
         <textarea {...register("message")} className="form-control" />
-        <p>{errors.message?.message}</p>
+        {!!errors.message?.message && <p>{errors.message?.message}</p>}
         <input type="submit" className="button-primary" value="Send" />
       </form>
+      <div
+        className={
+          isSubmitted ? "form-submit-status success" : "form-submit-status"
+        }
+      >
+        Your message has been sent successfully!
+      </div>
     </div>
   );
 };
